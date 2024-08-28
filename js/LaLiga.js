c2fea68edd319274c67609743429ae865459298e
BogdanDumitrascu04 <bogdandumitrascu0@gmail.com>
2024-08-28 22:47:10 +0300

    Update LaLiga.js

--- js/LaLiga.js
+++ js/LaLiga.js
@@ -45,5 +45,5 @@ closeButton.addEventListener('click', () => {
 
 adjustContentPosition();
 document.getElementById('startButton').onclick = function (){
-    window.location.assign("/LaLigaGame.html");
+    window.location.assign("LaLigaGame.html");
  };
